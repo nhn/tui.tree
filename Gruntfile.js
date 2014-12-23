5eb151298a4374ae944ec0ccc9cb2481159e9646
jein.yi <ahin1127>
2014-12-23 15:45:06 +0900

    package, buildfile added

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+//Grunt is just JavaScript running in node, after all...
+module.exports = function(grunt) {
+
+    // All upfront config goes in a massive nested object.
+    grunt.initConfig({
+        // You can set arbitrary key-value pairs.
+        distFolder: 'dist',
+        // You can also set the value of a key as parsed JSON.
+        // Allows us to reference properties we declared in package.json.
+        pkg: grunt.file.readJSON('package.json'),
+        // Grunt tasks are associated with specific properties.
+        // these names generally match their npm package name.
+        concat: {
+            // Specify some options, usually specific to each plugin.
+            options: {
+                banner: '(function() {\n',
+                footer: '\n})();'
+            },
+            // 'dist' is what is called a "target."
+            // It's a way of specifying different sub-tasks or modes.
+            dist: {
+                // The files to concatenate:
+                // Notice the wildcard, which is automatically expanded.
+                src: [
+                    'src/common/common.js',
+                    'src/js/tree.js',
+                    'src/**/*.js'
+                ],
+                // The destination file:
+                // Notice the angle-bracketed ERB-like templating,
+                // which allows you to reference other properties.
+                // This is equivalent to 'dist/main.js'.
+                dest: '<%= distFolder %>/Component-Tree.js'
+                // You can reference any grunt config property you want.
+                // Ex: '<%= concat.options.separator %>' instead of ';'
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    '<%= distFolder %>/Component-Tree.min.js' : '<%= distFolder %>/Component-Tree.js'
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, flatten: true, src: ['src/css/*'], dest: '<%= distFolder %>/', filter: 'isFile'}
+                ]
+            }
+        },
+        zip: {
+            main: {
+                src: ['<%= distFolder %>/*'],
+                dest: '<%= distFolder %>/Component-Tree.zip'
+            }
+        }
+    }); // The end of grunt.initConfig
+
+    // We've set up each task's configuration.
+    // Now actually load the tasks.
+    // This will do a lookup similar to node's require() function.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-zip');
+
+    // Register our own custom task alias.
+    grunt.registerTask('build', ['concat', 'uglify', 'copy']);
+};
