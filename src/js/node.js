098ac9bc9968d700e37962a95a5e6e5cb56f59be
minkyu-yi <minkyu.yi@nhnent.com>
2016-01-09 23:46:40 +0900

    feat: Node class - #20

---
+++ src/js/node.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var states = require('./states').node,
+    util = require('./util');
+
+var lastId = 0,
+    createId = function() {
+        lastId += 1;
+        return lastId;
+    };
+
+/**
+ * Node
+ * @Constructor Node
+ * @param {Object} nodeData - Node data
+ * @param {number} parentId - Parent node id
+ * @param {string} state - State of node
+ */
+var Node = tui.util.defineClass(/** @lends Node.prototype */{ /*eslint-disable*/
+    init: function(nodeData, parentId, state) { /*eslint-enable*/
+        /**
+         * Node id
+         * @type {number}
+         * @private
+         */
+        this._id = createId();
+
+        /**
+         * Parent node id
+         * @type {number}
+         * @private
+         */
+        this._parentId = parentId;
+
+        /**
+         * Id list of children
+         * @type {Array}
+         * @private
+         */
+        this._childIds = [];
+
+        /**
+         * Node data
+         * @type {object}
+         * @private
+         */
+        this._data = nodeData || {};
+
+        /**
+         * Node state
+         * @type {string}
+         * @private
+         */
+        this._state = states.CLOSED;
+        this.setState(state);
+    },
+
+    /**
+     * Toggle state
+     */
+    toggleState: function() {
+        if (this._state === states.CLOSED) {
+            this._state = states.OPENED;
+        } else {
+            this._state = states.CLOSED;
+        }
+    },
+
+    /**
+     * Set state
+     * @param {string} state - State of node ('closed', 'opened')
+     */
+    setState: function(state) {
+        state += '';
+        this._state = states[state.toUpperCase()] || this._state;
+    },
+
+    /**
+     * Get state
+     * @returns {string} state ('opened' or 'closed')
+     */
+    getState: function() {
+        return this._state;
+    },
+
+    /**
+     * Get id
+     * @returns {number} Node id
+     */
+    getId: function() {
+        return this._id;
+    },
+
+    /**
+     * Get parent id
+     * @returns {number} Parent node id
+     */
+    getParentId: function() {
+        return this._parentId;
+    },
+
+    /**
+     * Set parent id
+     * @param {number} parentId - Parent node id
+     */
+    setParentId: function(parentId) {
+        this._parentId = parentId;
+    },
+
+    /**
+     * Get id list of children
+     * @returns {Array.<number>} Id list of children
+     */
+    getChildIds: function() {
+        return this._childIds.slice(0);
+    },
+
+    /**
+     * Add child id
+     * @param {number} id - Child node id
+     */
+    addChild: function(id) {
+        var childIds = this._childIds;
+
+        if (tui.util.inArray(childIds, id) === -1) {
+            childIds.push(id);
+        }
+    },
+
+    /**
+     * Remove child id
+     * @param {number} id - Child node id
+     */
+    removeChild: function(id) {
+        util.removeItemFromArray(id, this._childIds);
+    },
+
+    /**
+     * Get data
+     * @returns {Object} Data
+     */
+    getData: function() {
+        return tui.util.extend({}, this._data);
+    },
+
+    /**
+     * Add data
+     * @param {Object} data - Data for adding
+     */
+    addData: function(data) {
+        tui.util.extend(this._data, data)
+    },
+
+    /**
+     * Remove data
+     * @param {...string} names - Names of data
+     */
+    removeData: function(names) {
+        names = tui.util.toArray(arguments);
+        tui.util.forEach(names, function(name) {
+            delete this._data[name]
+        }, this);
+    }
+});
+module.exports = Node;
---
+++ test/node.test.js
@@ -0,0 +1,61 @@
+
+'use strict';
+
+var Node = require('../src/js/node');
+
+describe('Node', function() {
+    var node,
+        parentId = 0,
+        state = 'closed',
+        data = {
+            title: 'My node'
+        };
+
+    beforeEach(function() {
+        node = new Node(data, parentId, state);
+    });
+
+    it('Methods about state', function() {
+        expect(node.getState()).toEqual(state);
+
+        node.setState('opened');
+        expect(node.getState()).toEqual('opened');
+
+        node.setState('asdf');
+        expect(node.getState()).toEqual('opened');
+
+        node.toggleState();
+        expect(node.getState()).toEqual('closed');
+    });
+
+    it('Methods about id', function() {
+        expect(node.getId()).toEqual(jasmine.any(Number));
+
+        expect(node.getParentId()).toEqual(0);
+
+        node.setParentId(-1);
+        expect(node.getParentId()).toEqual(-1);
+
+        node.addChild(4);
+        expect(node.getChildIds()).toContain(4);
+
+        node.removeChild(4);
+        expect(node.getChildIds()).not.toContain(4);
+    });
+
+    it('Methods about data', function() {
+        expect(node.getData()).toEqual(jasmine.objectContaining({
+            title: 'My node'
+        }));
+
+        node.addData({newData: 'data2'});
+        expect(node.getData()).toEqual(jasmine.objectContaining({
+            title: 'My node',
+            newData: 'data2'
+        }));
+
+        node.removeData('title', 'newData');
+        expect(node.getData().title).toBeUndefined();
+        expect(node.getData().newData).toBeUndefined();
+    });
+});
