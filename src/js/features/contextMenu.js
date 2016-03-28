6cec1d4190a74f4367f25de232d0d80cc16239a1
minkyu-yi <minkyu.yi@nhnent.com>
2016-03-28 10:43:21 +0900

    feat: stub - tree contextMenu

--- src/js/features/contextMenu.js
+++ src/js/features/contextMenu.js
@@ -15,30 +15,57 @@ var ContextMenu = tui.util.defineClass(/** @lends ContextMenu.prototype */{/*esl
         }
     },
 
-    init: function(tree, options) { /*eslint-enable*/
+    init: function(tree, menus) { /*eslint-enable*/
         this.tree = tree;
+        this.isOpened = false;
+        this.layer = document.createElement('DIV');
 
+        this.makeLayer(menus);
         this.attachEvent();
+    },
+
+    makeLayer: function(menus) {
+        var style = this.layer.style;
+
+        style.position = 'absolute';
+        style.backgroundColor = 'white';
+        style.border = '1px solid black';
+        this.layer.innerHTML = 'test';
     },
 
     attachEvent: function() {
         var tree = this.tree;
 
         tree.on('_contextMenu', function(event) {
-            var nodeId = tree.getNodeIdFromElement(util.getTarget(event));
+            var nodeId = tree.getNodeIdFromElement(util.getTarget(event)),
+                layer = this.layer,
+                self = this;
 
             event.preventDefault();
-            console.log('contextMenu: ', nodeId);
+            layer.style.top = event.clientY + 10 + 'px';
+            layer.style.left = event.clientX + 10 + 'px';
+
+            if (!this.isOpened) {
+                this.isOpened = true;
+                document.body.appendChild(layer);
+
+                util.addEventListener(document.body, 'click', function closeLayer() {
+                    self.isOpened = false;
+                    document.body.removeChild(layer);
+                    util.removeEventListener(document.body, 'click', closeLayer);
+                });
+            }
+
             /**
              * @api
              * @events Tree#contextMenu
              * @param {object} data - Data
              *  @param {data.nodeId} nodeId - selected node id
              */
-            tree.fire('contextMenu', {
+            tree.fire('openContextMenu', {
                 nodeId: nodeId
             });
-        });
+        }, this);
     },
 
     destroy: function() {
