3538a50731cd93379ee542494cdde98d69aeef98
jajugoguma <jajugoguma@gmail.com>
2021-07-09 15:54:54 +0900

    chore: apply code review
    chore: rename variable from pos to helperRect

--- src/js/features/draggable.js
+++ src/js/features/draggable.js
@@ -205,10 +205,10 @@ var Draggable = defineClass(
       var mousePosX = mousePos[0];
       var mousePosY = mousePos[1];
       var helperStyle = this.helperElement.style;
-      var pos = this.helperElement.getBoundingClientRect();
+      var helperRect = this.helperElement.getBoundingClientRect();
 
-      helperStyle.top = mousePosY - pos.height * 2 + this.helperPos.y + 'px';
-      helperStyle.left = mousePosX - pos.width / 2 + this.helperPos.x + 'px';
+      helperStyle.top = mousePosY - helperRect.height * 2 + this.helperPos.y + 'px';
+      helperStyle.left = mousePosX - helperRect.width / 2 + this.helperPos.x + 'px';
       helperStyle.display = '';
     },
 
