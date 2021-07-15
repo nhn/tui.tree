bb388ff0c502f4f193cf8102577bb14851e5bc05
김대연 <jajugoguma@gmail.com>
2021-07-15 17:26:12 +0900

    fix: wrong position of drag helper position (fix #84) (#85)
    * fix: wrong position of drag helper position
    
    * chore: apply code review
    
    chore: rename variable from pos to helperRect
    
    * chore: apply code review
    
    chore: caching helperRect in mouse down handler
    
    * chore: change helper position
    
    * chore: remove unused property

--- src/js/features/draggable.js
+++ src/js/features/draggable.js
@@ -205,10 +205,9 @@ var Draggable = defineClass(
       var mousePosX = mousePos[0];
       var mousePosY = mousePos[1];
       var helperStyle = this.helperElement.style;
-      var pos = this.tree.rootElement.getBoundingClientRect();
 
-      helperStyle.top = mousePosY - pos.top + this.helperPos.y + 'px';
-      helperStyle.left = mousePosX - pos.left + this.helperPos.x + 'px';
+      helperStyle.top = mousePosY + window.pageYOffset + this.helperPos.y + 'px';
+      helperStyle.left = mousePosX + window.pageXOffset + this.helperPos.x + 'px';
       helperStyle.display = '';
     },
 
